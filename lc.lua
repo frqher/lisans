58cb54aae2efb96fbb03b6f8d235dedb4224b5b1
Drewski <20355378+frqher@users.noreply.github.com>
2021-10-06 14:16:44 +0400

    Update lc.lua

--- lc.lua
+++ lc.lua
@@ -3,7 +3,8 @@ ip = {
 	["45.155.125.20"] = "Epin Atheris#0479",
 	["51.255.105.230"] = "Epin D o r u k#2765",
 	["51.210.22.198"] = "Epin ﹤Anıl ÜNAL﹥#2133",
-	["89.35.52.75"] = "Epin Olimpos#3111",
+	["89.35.52.75"] = "Epin Olimpos#3111", --1 ci ip
+	["89.35.52.77"] = "Epin Olimpos#3111 2", --2 ci ip
 	
 	
 	--Ev sistemi
