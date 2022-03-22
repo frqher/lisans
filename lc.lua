f36be5231de968007b0b76f0e87f30f041830733
Drewski <20355378+frqher@users.noreply.github.com>
2022-03-22 18:04:43 +0400

    Update lc.lua

--- lc.lua
+++ lc.lua
@@ -2,8 +2,6 @@ ip = {
 	--Epin
 	["51.255.105.230"] = "Epin D o r u k#2765",
 	["51.210.22.198"] = "Epin ﹤Anıl ÜNAL﹥#2133",
-	["89.35.52.75"] = "Epin Olimpos#3111", --1 ci ip
-	["89.35.52.77"] = "Epin Olimpos#3111 2", --2 ci ip
 	
 	
 	--Ev sistemi
@@ -14,8 +12,6 @@ iplist = {
 	["E-pin"]={
 		["51.255.105.230"] = "Epin D o r u k#2765",
 		["51.210.22.198"] = "Epin ﹤Anıl ÜNAL﹥#2133",
-		["89.35.52.75"] = "Epin Olimpos#3111", --1 ci ip
-		["89.35.52.77"] = "Epin Olimpos#3111 2", --2 ci ip
 	},
 	["EvSistemi"]={
 		["51.255.105.230"] = "Ev D o r u k#2765",
